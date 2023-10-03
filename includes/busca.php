978ff6b4b395695c6bbc66ad8e12b14967585e36
rafaela663 <rafaelarodrigues8055@gmail.com>
2023-10-03 08:17:02 -0300

    blog

--- includes/busca.php
+++ includes/busca.php
@@ -1,6 +1,6 @@
 <form class="form-inline my-2 my-lg-0" method='get' action=' '>
     <input class="form-control mr-sm-2" type="search" name='busca'
         placeholder="Busca" arial-label="Busca">
-    <button class="btn btn-outline-sucess my-2 my-sm-0"
+    <button class="btn btn-outline-success my-2 my-sm-0"
         type="submit">Buscar</button>
 </form>
